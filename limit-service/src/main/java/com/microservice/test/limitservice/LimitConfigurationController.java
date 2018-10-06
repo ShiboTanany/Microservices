f519095c6a03c0ef6472a8127c688e67ff6be2cd
ShiboTanany <shaaban.altanany@gmail.com>
2018-10-06 16:32:46 +0200

    adding server configuration and edit on limit service

--- limit-service/src/main/java/com/microservice/test/limitservice/LimitConfigurationController.java
+++ limit-service/src/main/java/com/microservice/test/limitservice/LimitConfigurationController.java
@@ -1,14 +1,21 @@
 package com.microservice.test.limitservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	
+	
 	@GetMapping("/limits")
-	public limitConfiguration getLimitConfiguration() {
-		
-		return new limitConfiguration(656, 56);
+	public Configuration getLimitConfiguration() {
+
+		return configuration;
 	}
 
 }
